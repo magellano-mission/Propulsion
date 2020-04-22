3727e9cb9d039f821495b45a0c2f02798d23d60e
lyle-poli <58298534+lyle-poli@users.noreply.github.com>
2020-04-22 15:31:45 +0200

    Particular Case capability added
    Added some more motors to Motor_Data and added the ability to input a scalar value for dV and payload mass and get numerical results

--- Propulsion_Comparison/Propulsion_Comparison.m
+++ Propulsion_Comparison/Propulsion_Comparison.m
@@ -2,26 +2,47 @@
 clearvars
 close all
 clc
-
 [motors] = Motor_Data();            %Load all motor data
 
-mass_pay = linspace(0,1000,100);     %[kg] payload mass array
-delta_v = linspace(0,2000,100);      %[m/s] delta V of burn array
+% Input arrays of payload masses and delta-Vs to observe trends
+mass_pay = linspace(0,500,100);     %[kg] payload mass array
+dV = linspace(1500,2500,100);      %[m/s] delta V of burn array
 
-max_t_burn = inf;                   %[s]    maximum burn time allowable
-min_thrust = 0;                     %[N]    minimum thrust of interest
-max_thrust = 3000;                  %[N]    maximum thrust of interest
-max_power = 4000;                   %[W]    maximum power consumption allowable
+%Input a particular case to get numerical outputs
+payload = 250;  %[kg]
+delta_v = 200;  %[m/s]
+
+%Input boundary conditions to filter motors
+max_t_burn = 60*24*3600;            %[s]    maximum burn time allowable
+min_thrust = 0.1;                   %[N]    minimum thrust of interest
+max_thrust = 5000;                  %[N]    maximum thrust of interest
+min_isp = 290;                      %[s]    maximum isp of interest
+max_isp = inf;                      %[s]    minimum isp of interest
+max_power = 2e5;                   %[W]    maximum power consumption allowable
+classes = ["chemical biprop", "ion", "hall effect"];
+% all classes - "chemical monoprop", "chemical biprop", "ion", "chemical solid", "hall effect"
 
 %% FILTERING OF INPUT DATA
 %Incompatible motors are filtered by thrust and power constraints.
 %Thrust is throttled down if the motor maximum thrust is too high or the
 % power draw is too high.
-%Maximum time constraint is simply applied as a z axis limit to the burn
-% time plot.
+%Maximum time constraint is simply applied as a reference surface in the
+%burn time plot
 
 i = 1;
 while i <= length(motors)   
+    %removing motor classes that aren't acceptable
+    if ismember(motors(i).class, classes) == 0
+        motors(i) = [];  
+        continue
+    end    
+        
+    %removing motor if isp doesn't fall within acceptable range
+    if (min_isp > motors(i).isp) || (motors(i).isp > max_isp)
+        motors(i) = [];  
+        continue
+    end    
+    
     %removing motor if thrust doesn't fall within acceptable range
     if (min_thrust > motors(i).thrust_max) || (motors(i).thrust_min > max_thrust)
         motors(i) = [];  
@@ -67,13 +88,21 @@ end
 % Creating an array of default MATLAB colours for the surface plots to use
 % Need to add more or use random colours if large number of motors will be
 % plotted at one time
-colours = [1 1 0; 1 0 1; 0 1 1; 1 0 0; 0 1 0; 0 0 1; 1 1 1; 0 0 0; 0, 0.4470, 0.7410; ...
+colours = [1 0 0; 1 0 1; 0 1 1; 1 1 0; 0 1 0; 0 0 1; 0, 0.4470, 0.7410; ...
     0.4940, 0.1840, 0.5560; 0.4660, 0.6740, 0.1880; 0.6350, 0.0780, 0.1840; 0.25, 0.25, 0.25];
 %% CALCULATE SYSTEM PROPERTIES
 clearvars i
-for i = 1:length(motors)            %calculate mass, volume and burn time
-    [motors(i).mass_sys, motors(i).vol_sys, motors(i).time_sys] = ...
-            sys_eval(mass_pay,delta_v,motors(i));
+for i = 1:length(motors)  %calculate mass, volume and burn time ARRAYS
+    [motors(i).mass_sys, motors(i).mass_prop, motors(i).vol_prop, motors(i).time_sys] = ...
+            sys_eval(mass_pay,dV,motors(i));
+end
+
+for i = 1:length(motors)  %calculate mass, volume, and burn time PARTICULAR CASE VALUES
+    [motors(i).case.mass_sys, motors(i).case.prop_mass, motors(i).case.prop_vol, motors(i).case.burn_time_sec] = sys_eval(payload,delta_v,motors(i));
+    motors(i).case.burn_time_hrs = motors(i).case.burn_time_sec/3600;
+    motors(i).case.burn_time_day = motors(i).case.burn_time_hrs/24;
+    disp(motors(i).name)
+    disp(motors(i).case)
 end
 %% PROPULSION STAGE MASS PLOT
 %Plots the sum of propellant mass and motor hardware. Mass of plumbing,
@@ -82,7 +111,7 @@ subplot(2,2,1)
 hold on
 clearvars i
 for i = 1:length(motors)
-    surf(mass_pay,delta_v,motors(i).mass_sys,'LineStyle', 'none','FaceColor',colours(i,:))
+    surf(mass_pay,dV,motors(i).mass_sys,'LineStyle', 'none','FaceColor',colours(i,:))
 end
 xlabel('Payload Mass (kg)')
 ylabel('Burn Delta V (m/s)')
@@ -98,7 +127,7 @@ subplot(2,2,2)
 hold on
 clearvars i
 for i = 1:length(motors)
-    surf(mass_pay, delta_v, motors(i).vol_sys, 'LineStyle', 'none','FaceColor',colours(i,:))
+    surf(mass_pay, dV, motors(i).vol_prop, 'LineStyle', 'none','FaceColor',colours(i,:))
 end
 xlabel('Payload Mass (kg)')
 ylabel('Burn Delta V (m/s)')
@@ -115,24 +144,25 @@ subplot(2,2,3)
 hold on
 clearvars i
 for i = 1:length(motors)
-    surf(mass_pay, delta_v, motors(i).time_sys, 'LineStyle', 'none','FaceColor',colours(i,:))
+    surf(mass_pay, dV, motors(i).time_sys, 'LineStyle', 'none','FaceColor',colours(i,:),'HandleVisibility','off')
 end
-
+max_time = max_t_burn*ones(length(mass_pay),length(dV));
+surf(mass_pay, dV, max_time, 'LineStyle', 'none','FaceColor',[0 0 0],'DisplayName','Maneuver Time Limit' )
+legend()
 xlabel('Payload Mass (kg)')
 ylabel('Burn Delta V (m/s)')
 zlabel('Burn Time (s)')
 view(45,15)
-zlim([0 max_t_burn])
+zlim([0 inf])
 axis vis3d
 grid on
 set(gca,'ZScale','log')
 hold off
 %% DUMMY PLOT
-% This is just a shitty workaround in order to display the legend
-% separately in the display window
+% Shitty workaround in order to display the legend separate to figures
 blank1 = NaN(1,length(mass_pay));
-blank2 = NaN(1,length(delta_v));
-blank3 = NaN (length(mass_pay),length(delta_v));
+blank2 = NaN(1,length(dV));
+blank3 = NaN (length(mass_pay),length(dV));
 
 subplot(2,2,4)
 hold on
--- Propulsion_Comparison/sys_eval.m
+++ Propulsion_Comparison/sys_eval.m
@@ -1,23 +1,23 @@
-function [mass_sys, vol_sys, time_sys] = sys_eval(mass_pay,delta_v,motors)
+function [mass_sys, mass_prop, vol_sys, time_sys] = sys_eval(mass_pay,dV,motors)
 
 % sys_eval takes the input vectors for payload mass and burn delta v and
 % calculates the propulsion system mass (motor hardware + propellant),
 % propellant volume and motor burn time
 
 g0 = 9.81;  %[m/s2] 
-mass_dry = mass_pay + motors.mass;    %[kg]
+mass_dry = mass_pay  + motors.mass;    %[kg]
 
-mass_init = zeros(length(mass_dry),length(delta_v));
-mass_prop = zeros(length(mass_dry),length(delta_v));
-mass_sys = zeros(length(mass_dry),length(delta_v));
-time_sys = zeros(length(mass_dry),length(delta_v));
-vol_sys = zeros(length(mass_dry),length(delta_v));
+mass_init = zeros(length(mass_dry),length(dV));
+mass_prop = zeros(length(mass_dry),length(dV));
+mass_sys = zeros(length(mass_dry),length(dV));
+time_sys = zeros(length(mass_dry),length(dV));
+vol_sys = zeros(length(mass_dry),length(dV));
 
 for i = 1:length(mass_dry)
-    for j = 1:length(delta_v)
-        mass_init(i,j) = mass_dry(i) .* exp(delta_v(j) ./ (motors.isp*g0));
+    for j = 1:length(dV)
+        mass_init(i,j) = mass_dry(i) .* exp(dV(j) ./ (motors.isp*g0));
         mass_prop(i,j) = mass_init(i,j) - mass_dry(i);
-        mass_sys(i,j) = motors.mass + mass_prop(i,j);
+        mass_sys(i,j) = mass_prop(i,j)  + motors.mass;
         vol_sys(i,j) = mass_prop(i,j) / motors.prop_dens;
         motors.mdot = motors.thrust / (motors.isp*g0);
         time_sys(i,j) = mass_prop(i,j) / motors.mdot;
