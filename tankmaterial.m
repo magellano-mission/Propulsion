f9ae99641d6017c6fbf6c2ea0375c6bf83e1fcd8
Alice <AliDott@users.noreply.github.com>
2020-05-07 11:34:34 +0200

    Update tankmaterial.m
    CFRP added

--- tankmaterial.m
+++ tankmaterial.m
@@ -4,6 +4,7 @@ function [rho_m,sigma_tum]=tankmaterial(material)
 %   Stainless steel
 %   Alloy steel
 %   Ti6Al4V
+%   Ti15-3
 
 switch material
     case 'Al2024T3' 
@@ -18,5 +19,12 @@ switch material
     case 'Ti6Al4V'
         rho_m = 4430; %kg/m3
         sigma_tum = 950e6; %Pa
+    case 'Ti15-13'
+        rho_m = 4760; %kg/m3
+        sigma_tum = 703e6; %Pa
+    case 'CFRP+Al' %"Mechanical properties of carbon fibre reinforced aluminium laminates using two different layering pattern for aero engine application", October 2018, DOI: 10.1080/2374068X.2018.1530427, Ibrahim Mohammed, Abd Rahim Abu Talib
+        rho_m = 2174.18;%7Al+17CFRP
+        sigma_tum = 350e6;
+
 end
 end
